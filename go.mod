db09f14b7be7a35399c862d3f7dbc98f5cfcb685
manisankar-divi <161689361+manisankar-divi@users.noreply.github.com>
2024-10-18 16:26:33 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/manisankar-divi/go-project
+
+go 1.23.2
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+// TestHelloHandler tests the helloHandler function.
+func TestHelloHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(helloHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := "Hello, World!" // Update expected output to NOT include newline
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
